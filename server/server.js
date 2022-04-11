5a334624d330ed72526651b1b24f29d19e37e07f
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-04-11 13:00:43 -0400

    server updates

--- server/server.js
+++ server/server.js
@@ -2,17 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors({ origin: 'https://johndemployment-tracker.netlify.app' }));
 
 app.get('/', (req, res) => {
-  res.send('Test Deployment Success')
-})
+  res.send('Test Deployment Success');
+});
 
 mongoose.connect(
-  'mongodb://localhost:27017/employeetracker',
+  process.env.mongodb_uri || 'mongodb://localhost:27017/employeetracker',
   console.log('Connected to employee tracker database')
 );
 
@@ -21,6 +23,14 @@ app.use('/employees', require('./routes/employeeRoutes'));
 app.use((err, req, res, next) => {
   console.log(err);
   return res.send({ error: err.message });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (err.name === 'UnauthorizedError') {
+    res.status(err.status);
+  }
+  return res.send({ errMsg: err.message });
 });
 
 app.listen(process.env.PORT || 9000, () => {
