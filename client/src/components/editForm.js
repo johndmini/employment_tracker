c99f336610455982dccc33f859364edea3f12dd4
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-04-11 17:32:13 -0400

    styling fix update

--- client/src/components/editForm.js
+++ client/src/components/editForm.js
@@ -1,69 +1,72 @@
 import React from 'react';
+import '../Styles/editForm.css';
 
 export default function Edit(props) {
   return (
-    <form className="edit-form" onSubmit={props.onSubmit}>
-      <div>
-        <label>
-          First Name:
-          <input
-            name="firstName"
-            value={props.firstName}
-            onChange={props.handleChange}
-          />
-        </label>
+    <form onSubmit={props.onSubmit}>
+      <div className="additional-info">
+        <div>
+          <label>
+            First Name:
+            <input
+              name="firstName"
+              value={props.firstName}
+              onChange={props.handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Last Name:
+            <input
+              name="lastName"
+              value={props.lastName}
+              onChange={props.handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email:
+            <input
+              name="email"
+              value={props.email}
+              onChange={props.handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Phone Number:
+            <input
+              name="phone"
+              value={props.phone}
+              onChange={props.handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Department:
+            <select onChange={props.handleSelect}>
+              <option value="IT Development">IT Development</option>
+              <option value="IT Maintenance">IT Maintenance</option>
+              <option value="IT QA/QC">IT QA/QC</option>
+            </select>
+          </label>
+        </div>
+        <div>
+          <label>
+            Mailing Address:
+            <input
+              name="address"
+              value={props.address}
+              onChange={props.handleChange}
+            />
+          </label>
+        </div>
       </div>
-      <div>
-        <label>
-          Last Name:
-          <input
-            name="lastName"
-            value={props.lastName}
-            onChange={props.handleChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Email:
-          <input
-            name="email"
-            value={props.email}
-            onChange={props.handleChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Phone Number:
-          <input
-            name="phone"
-            value={props.phone}
-            onChange={props.handleChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Department:
-          <select onChange={props.handleSelect}>
-            <option value="IT Development">IT Development</option>
-            <option value="IT Maintenance">IT Maintenance</option>
-            <option value="IT QA/QC">IT QA/QC</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          Mailing Address:
-          <input
-            name="address"
-            value={props.address}
-            onChange={props.handleChange}
-          />
-        </label>
-      </div>
-      <div>
+      <div className="emergencyContact">
         <h5>Emergency Contact:</h5>
         <label>
           First Name:
--- client/src/pages/addEmployeePage.js
+++ client/src/pages/addEmployeePage.js
@@ -21,7 +21,7 @@ export default function NewEmployeePage(props) {
 
   const addEmployee = () => {
     axios
-      .post('https://employment-tracker.herokuapp.com/employees', newEmployee)
+      .post('https://employment-tracker.herokuapp.com/employees' || '/employees', newEmployee)
       .then((res) => setEmployees((prevState) => [...prevState, res.data]))
       .catch((err) => console.log(err));
     setNewEmployee(initialInput);
